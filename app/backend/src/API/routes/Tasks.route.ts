79c82bcc9a3d63eed387e27a7a1fdc9d39594bac
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-05-19 20:05:02 -0300

    feat: create routes to use in the API

---
+++ app/backend/src/API/routes/Tasks.route.ts
@@ -0,0 +1,38 @@
+import { Request, Response, Router } from 'express';
+
+import TasksController from '../controllers/Tasks.controller';
+import TasksBoardService from '../services/Tasks.service';
+
+const tasksController = new TasksController(new TasksBoardService());
+
+const tasksBoardRouter = Router();
+
+tasksBoardRouter.get(
+  '/',
+  async (req: Request, res: Response) => {
+    await tasksController.findAll(req, res);
+  },
+);
+
+tasksBoardRouter.post(
+  '/',
+  async (req: Request, res: Response) => {
+    await tasksController.create(req, res);
+  },
+);
+
+tasksBoardRouter.patch(
+  '/:id',
+  async (req: Request, res: Response) => {
+    await tasksController.update(req, res);
+  },
+);
+
+tasksBoardRouter.delete(
+  '/:id',
+  async (req: Request, res: Response) => {
+    await tasksController.remove(req, res);
+  },
+);
+
+export default tasksBoardRouter;
