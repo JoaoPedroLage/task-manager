69acd04ae3fd6e2266be5b6e633df337831df61e
JoaoPedroLage <joaopedro.lage@outlook.com>
2023-03-17 16:37:49 -0300

    feat: change the request url from localhost to the url of the AWS vm with the ip 18.231.45.178

--- app/front-end/src/services/requests.js
+++ app/front-end/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'http://18.231.45.178:3001',
 });
 
 export const requestData = async (endpoint, body) => {
