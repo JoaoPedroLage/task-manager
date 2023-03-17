0a42730460342985490929660e4b2a0241a33466
JoaoPedroLage <joaopedro.lage@outlook.com>
2023-03-17 15:02:43 -0300

    refactor: renames folders, places the backend in a separate folder and renames the file .env

--- app/database/config/database.ts
+++ app/database/config/database.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
+  username: 'root',
+  password:  'mysql098',
+  database:  'task_manager',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 3036,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
