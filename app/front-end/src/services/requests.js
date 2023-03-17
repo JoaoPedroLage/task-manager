f01909777fe8717aee5ea77f80cf1927a3b7a453
JoaoPedroLage <joaopedro.lage@outlook.com>
2023-03-17 17:57:37 -0300

    feat: add a ngrok tunnel with https to work with deploy in AWS

--- app/front-end/src/services/requests.js
+++ app/front-end/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://18.231.45.178:3001',
+  baseURL: 'https://429f-18-231-45-178.ngrok.io/',
 });
 
 export const requestData = async (endpoint, body) => {
