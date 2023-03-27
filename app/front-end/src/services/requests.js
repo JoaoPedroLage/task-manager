e4f61ad9c179c64536f49b6ea428da14b72fadd3
JoaoPedroLage <joaopedro.lage@outlook.com>
2023-03-27 16:33:47 -0300

    refactor: tries to resolve crash in deploy

--- app/front-end/src/services/requests.js
+++ app/front-end/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://077e-54-232-16-28.ngrok.io',
+  baseURL: 'https://39be-54-232-16-28.ngrok.io',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
